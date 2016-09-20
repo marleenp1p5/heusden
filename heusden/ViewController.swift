5283ef5df30b8ec7500087bd1af05dbeab26d2ae
Marleen Berg <marleen@metmeer.nl>
2016-09-20 22:37:15 +0200

    first commit

---
+++ heusden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heusden
+//
+//  Created by Marleen Berg on 20-09-16.
+//  Copyright © 2016 p1p5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
